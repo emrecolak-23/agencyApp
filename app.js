14a2ad79f318a74530c1fad3cad34a5cb9b90052
Emre Çolak <colakkemre@gmail.com>
2022-03-29 15:34:54 +0300

    database connection completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import Packages
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 // Import Routes
 const PageRouter = require("./routes/PageRoute");
@@ -13,16 +14,27 @@ app.set("view engine","ejs");
 
 // middlewares
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Page Routes
 app.use("/",PageRouter);
 
 
-// Declare PORT
-const PORT = 14000
-
-// Listen Server
-app.listen(PORT, () => {
-  console.log(`Server listened in ${PORT} port`);
-})
+// Connect DB
+const dbURI =
+  'mongodb+srv://emco:emco3232@nodetuts.iuulr.mongodb.net/agencyApp?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("database connected")
+    // declare port number
+    const PORT = process.env.PORT || 14000;
+    // listen for request
+    app.listen(PORT, ()=>{
+      console.log("Server listened")
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
