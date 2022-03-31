620ba9efea3ce15fe1d157cfde21439894121d77
Emre Çolak <colakkemre@gmail.com>
2022-03-31 11:06:19 +0300

    static pages controller and routes completed

--- controllers/PageController.js
+++ controllers/PageController.js
@@ -1,26 +1,41 @@
 // Import Models
-const Category = require("../models/Category");
-const Project = require("../models/Project");
-const Client = require("../models/Client");
+const Category = require('../models/Category');
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
 exports.getHomePage = async (req, res) => {
+  const projects = await Project.find()
+    .sort('-createdAt')
+    .populate('category')
+    .populate('client');
 
-  const projects = await Project.find().sort("-createdAt")
-                                       .populate("category")
-                                       .populate("client")
-
-  res.status(200).render("index", {
-    projects
+  res.status(200).render('index', {
+    projects,
   });
-}
+};
 
-exports.getAddPage = async (req,res) => {
-
+exports.getAddPage = async (req, res) => {
   const categories = await Category.find();
   const clients = await Client.find();
 
-  res.status(200).render("add", {
+  res.status(200).render('add', {
     categories,
-    clients
+    clients,
   });
-}
+};
+
+exports.getServicePage = (req, res) => {
+  res.status(200).render('services');
+};
+
+exports.getAboutPage = (req, res) => {
+  res.status(200).render('about');
+};
+
+exports.getTeamPage = (req, res) => {
+  res.status(200).render('team');
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact');
+};
--- routes/PageRoute.js
+++ routes/PageRoute.js
@@ -1,13 +1,17 @@
 // Import packages
-const express = require("express");
+const express = require('express');
 
 // Import Controller
-const PageController = require("../controllers/PageController");
-const ProjectController = require("../controllers/ProjectController");
+const PageController = require('../controllers/PageController');
+const ProjectController = require('../controllers/ProjectController');
 
 const router = express.Router();
 
-router.route("/add").get(PageController.getAddPage);
-router.route("/").get(PageController.getHomePage);
+router.route('/add').get(PageController.getAddPage);
+router.route('/').get(PageController.getHomePage);
+router.route('/services').get(PageController.getServicePage);
+router.route('/about').get(PageController.getAboutPage);
+router.route('/team').get(PageController.getTeamPage);
+router.route('/contact').get(PageController.getContactPage);
 
-module.exports = router
+module.exports = router;
